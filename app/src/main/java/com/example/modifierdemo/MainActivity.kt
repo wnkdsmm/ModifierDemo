1d825b3572718183b14107a9c24a51527c2e8f1f
wnkdsmm <lwjfkg@mail.ru>
2025-10-06 09:51:06 +0700

    CustomImage

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,8 +37,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
     val mymodifier = modifier
-        .border(width = 2.dp, color = Color.Black)
         .padding(all = 10.dp)
+        .border(width = 2.dp, color = Color.Black)
+
 
     Text(
         "Hello Compose",
@@ -52,4 +55,12 @@ fun DefaultPreview() {
     ModifierDemoTheme {
         DemoScreen()
     }
+}
+
+@Composable
+fun CustomImage(image: Int, modifier: Modifier) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = null
+    )
 }
